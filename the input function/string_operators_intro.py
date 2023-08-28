1ea79d5b38f53f3d279cb2d0687ef3082052edfc
Malcolm Chikadibia Iheremelam <malcolmihere@outlook.com>
2023-08-28 07:16:16 +0100

    string_operators_intro.py (updated)

--- the input function/string_operators_intro.py
+++ the input function/string_operators_intro.py
@@ -13,7 +13,7 @@ The + (plus) sign, when applied to two strings, becomes a concatenation operator
 string + string
 
 It simply concatenates (glues) two strings into one. Of course, like its arithmetic sibling,
-it can be used more than once in one expression, and in such a context it behaves according to left-sided binding.
+It can be used more than once in one expression, and in such a context it behaves according to left-sided binding.
 
 In contrast to its arithmetic sibling, the concatenation operator is not commutative, i.e., "ab" + "ba" is not the same as "ba" + "ab".
 
@@ -30,7 +30,8 @@ print("Thank you.")
 print("\nYour name is " + fnam + " " + lnam + ".")
 
 """
-Note: using + to concatenate strings lets you construct the output in a more precise way than with a pure print() function,
+Note: Using + to concatenate strings lets you construct the output in a more precise way than with a pure print() function,
 even if enriched with the end= and sep= keyword arguments.
 
 Run the code and see if the output matches your predictions.
+"""
